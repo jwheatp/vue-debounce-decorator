458dff264ca5ab241d3c40beb67c3e7da2c473e2
Julien B <julien.blegean@gmail.com>
2021-12-13 14:45:52 +0100

    added lib

--- src/vue-debounce-decorator.ts
+++ src/vue-debounce-decorator.ts
@@ -24,6 +24,7 @@ export function Debounce(options: DebounceOptions): MethodDecorator {
 
     opts.methods[handler] = function(...args: any[]) {
       clear()
+      //@ts-ignore
       timeoutId = setTimeout(() => {
         timeoutId = 0
         originalFn.apply(this, args)
